f9ab7e4c81be4e2f7daf457f7d20044c0fad4eff
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2023-05-11 16:49:06 +1000

    Sets import to type

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,7 +1,7 @@
 import { useContext, $ } from "@builder.io/qwik";
 import { type RequestDocument, type Variables } from "graphql-request";
 import { ClientContext } from "../components/client-provider";
-import { TypedDocumentNode } from "@graphql-typed-document-node/core";
+import { type TypedDocumentNode } from "@graphql-typed-document-node/core";
 import { print } from "graphql";
 
 export default function useQuery<T = unknown, V extends Variables = Variables>(
